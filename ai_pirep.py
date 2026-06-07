5ed7337233ed301c9972d1e93dc43f93aebc0e0e
FADM ECNM CORY ANDREW HOFSTAD USN <96269532+FADM-DCMN-CORY-A-HOFSTAD-USN@users.noreply.github.com>
2026-06-07 00:20:45 -0700

    Update ai_pirep.py

--- ai_pirep.py
+++ ai_pirep.py
@@ -2,7 +2,6 @@
 import pyttsx3 
 
 # --- PRIMARY ENGINE: [Model Name] ---
-import streamlit as st
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -13,6 +12,7 @@ import aviation_telemetry      # Data flow
 import aircraft_perf           # Performance calculations
 import sensor_thermodynamics   # Env data scaling
 import aerodynamic_matrix      # Lift/Drag logic
+import streamlit as st
 
 def generate_pirep_data(live_data, user_inputs):
     """

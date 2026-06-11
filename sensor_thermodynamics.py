6f22311eab777e647a4977636aeff8677346cc8b
FADM ECNM CORY ANDREW HOFSTAD USN <96269532+FADM-DCMN-CORY-A-HOFSTAD-USN@users.noreply.github.com>
2026-06-10 22:07:59 -0700

    Update sensor_thermodynamics.py

--- sensor_thermodynamics.py
+++ sensor_thermodynamics.py
@@ -1,6 +1,5 @@
 import aviation_physics
 import multiprocessing as mp
-import numpy as np
 try:
     import cupy as xp
     HAS_GPU = True
@@ -10,14 +9,12 @@ except ImportError:
     HAS_GPU = False
 import numba
 from numba import njit
-@njit(fastmath=True)
-import pandas as pd
 import matplotlib.pyplot as plt
 import aircraft_perf
 import aviation_telemetry
 import sensor_thermodynamics
-import streamlit as st
 import aerodynamic_matrix
+@njit(fastmath=True)
 def calculate_wet_sensor_penalty(telemetry_override=None, t_ambient_c, humidity, wind_speed_mps, is_wooden_sensor=False, is_raining=True):
     """
     Adjusts the predicted official maximum temperature downward due to 
@@ -32,6 +29,7 @@ def calculate_wet_sensor_penalty(telemetry_override=None, t_ambient_c, humidity,
     delta_t_evap_c = (L_v * e_rate / (h_c * 1000)) * material_modifier
     delta_t_evap_f = delta_t_evap_c * (9.0/5.0)
     return round(delta_t_evap_f, 2)
+@njit(fastmath=True)
 def calculate_magnetic_field_cooling(b_field_tesla=5.3e-5, air_density=1.2, humidity=50.0):
     """
     Calculates the theoretical diamagnetic cooling effect of a local magnetic field 
